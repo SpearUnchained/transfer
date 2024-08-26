bc5da48703ac2051e2943249af184c3413bc3867
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2024-08-27 00:17:56 +0600

    New Update for All params

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     
     const urlParams = new URLSearchParams(window.location.search);
     const transactionHash = urlParams.get('transactionHashes');
-    const transactionTye = urlParams.get('type');
+
+    const ISTEST = true;
     function shortenTxnHash(txnHash, charsToShow = 6) {
         if (txnHash.length <= charsToShow * 2) {
             return txnHash; // If already short, no need to change
@@ -20,22 +20,25 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('txnLink').setAttribute('href', txnLink);
     }
     
-    const txnLink = `https://testnet.nearblocks.io/txns/${transactionHash}`;
+    const txnLink = `${ISTEST? "https://testnet.nearblocks.io/txns/":"https://nearblocks.io/txns/"}${transactionHash}`;
     
     // Update transaction details on page load
     updateTransactionDetails(transactionHash, txnLink);
-    
+
+    // Collect all URL parameters into a data object
+    const data = {};
+    urlParams.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    // Add additional details
+    data.status = "success";
+    data.txnLink = transactionHash;
+
     // Add event listener to continue button
     document.getElementById('continueBtn').addEventListener('click', function() {
-        const data = {
-            type:transactionTye,
-            status: "success",
-            txnLink: transactionHash
-        }
         const tg = window.Telegram.WebApp;
         tg.sendData(JSON.stringify(data));
     });
 
 });
-
-
